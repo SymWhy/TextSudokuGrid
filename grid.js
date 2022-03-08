f296738fbe24239f2f3168c3cb9a8334fb331597
SymWhy <simone.whysel@gmail.com>
2022-03-08 10:06:29 -0500

    create inner grid

--- grid.js
+++ grid.js
@@ -5,10 +5,20 @@ let listOfGrids = [];
 
 function buildGrid(i) {
     myDigit = '☐';
+    let listofChars = ['A','B','C','D','E','F','G','H','I'];
     for (let i = 0; i < 9; i++) {
         let myGrid = document.createElement('div');
-        myGrid.id = 'subgrid' + i;
-        myGrid.innerText = myDigit;
+        let listofBoxes = [];
+        myGrid.id = 'grid' + listofChars[i] + i;
+        for (let j = 0; j < 9; j++) {
+            let myBox = document.createElement('div');
+            //unicode escape sequence?
+            myBox.id = 'numbox' + listofChars[j] + j;
+            myBox.innerText = myDigit;
+            myGrid.appendChild(myBox);
+            listofBoxes.push(myBox);
+        }
+        console.log(listofBoxes);
             grid9x9.appendChild(myGrid);
             listOfGrids.push(myGrid);
     }
